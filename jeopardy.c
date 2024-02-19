3771f100fad240037506398ba4175d6621e6ce53
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-19 18:12:00 -0500

    Update jeopardy.c

--- jeopardy.c
+++ jeopardy.c
@@ -10,28 +10,28 @@
 int main()
 {
     player players[NUM_PLAYERS];
-    char buffer[MAX_LEN];
+    char buffer[MAX_QUESTION_LEN];
 
-    initialize_game();
+    initialize_game(questions);
 
     for (int i = 0; i < NUM_PLAYERS; ++i)
     {
         printf("Enter the name of player %d: ", i + 1);
-        fgets(buffer, MAX_LEN, stdin);
+        fgets(buffer, MAX_PLAYER_NAME_LEN, stdin);
         buffer[strcspn(buffer, "\n")] = '\0';
         initialize_player(&players[i], buffer);
     }
 
     while (1)
     {
-        display_categories_and_values();
-        
+        display_categories(questions);
+
         // Implement game logic here
 
         // Check if the game has ended
-        if (is_game_over())
+        if (is_game_over(questions))
         {
-            show_results(players, NUM_PLAYERS);
+            show_results(players);
             break;
         }
     }
