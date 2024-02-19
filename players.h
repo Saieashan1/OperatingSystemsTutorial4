aaeb70ddd978b8c0104ba70b6a54521782537dd1
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-19 18:41:33 -0500

    Update players.h

--- players.h
+++ players.h
@@ -1,28 +1,20 @@
-/*
- * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2015, <GROUP MEMBERS>
- * All rights reserved.
- *
- */
+/* players.h */
+
 #ifndef PLAYERS_H_
 #define PLAYERS_H_
 
 #include <stdbool.h>
 
-#define MAX_LEN 256
+#define MAX_PLAYER_NAME_LEN 30
+#define NUM_PLAYERS 4
 
-// Player struct for each player
 typedef struct {
-    char name[MAX_LEN];
+    char name[MAX_PLAYER_NAME_LEN];
     int score;
 } player;
 
-// Returns true if the player name matches one of the existing players
-extern bool player_exists(player *players, int num_players, char *name);
-
-// Go through the list of players and update the score for the 
-// player given their name
-extern void update_score(player *players, int num_players, char *name, int score);
+extern bool player_exists(player *players, char *name);
+extern void update_score(player *players, char *name, int score);
+extern void initialize_player(player *player, char *name);
 
 #endif /* PLAYERS_H_ */
