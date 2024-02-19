3344be97b875d41bd0a6451e0a4a98b41853a001
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-19 18:07:23 -0500

    Update jeopardy.h

--- jeopardy.h
+++ jeopardy.h
@@ -1,19 +1,17 @@
-/*
- * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2015, <GROUP MEMBERS>
- * All rights reserved.
- *
- */
+/* jeopardy.h */
+
 #ifndef JEOPARDY_H_
 #define JEOPARDY_H_
 
 #define MAX_LEN 256
+#define NUM_PLAYERS 4
 
-// Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
+typedef struct {
+    char name[MAX_LEN];
+    int score;
+} player;
+
 extern void tokenize(char *input, char **tokens);
-
-// Displays the game results for each player, their name and final score, ranked from first to last place
 extern void show_results(player *players, int num_players);
 
 #endif /* JEOPARDY_H_ */
