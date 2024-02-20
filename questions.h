4acaec53ff72beb5c8039e15ea71b12c3ba281b8
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-20 18:31:11 -0500

    Update questions.h

--- questions.h
+++ questions.h
@@ -5,17 +5,20 @@
 
 #include <stdbool.h>
 
+#define MAX_CATEGORIES  // Define the maximum number of categories
+#define NUM_DOLLAR_VALUES  // Define the number of dollar values per category
 #define MAX_QUESTION_LEN 256
-#define NUM_DOLLAR_VALUES 5
 
+// Structure definition for a question
 typedef struct {
-    char category[MAX_CATEGORY_NAME_LEN];
+    char category[MAX_QUESTION_LEN];
     char question[MAX_QUESTION_LEN];
     char answer[MAX_QUESTION_LEN];
     int value;
     bool answered;
 } question;
 
+// Function prototypes
 extern void initialize_game(question *questions);
 extern void display_categories(question *questions);
 extern void display_question(question *questions, char *category, int value);
