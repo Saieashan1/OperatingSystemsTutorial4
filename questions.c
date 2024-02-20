b59f6e1bb4d6630c3ac8c9ab045cf6df3ffc2c8b
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-20 18:44:35 -0500

    Update questions.c

--- questions.c
+++ questions.c
@@ -9,28 +9,28 @@
 
 static question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES];
 
-void initialize_game(question *questions)
+void initialize_game(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES])
 {
     // Implement initialization of questions array
 }
 
-void display_categories(question *questions)
+void display_categories(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES])
 {
     // Implement display of remaining categories and question dollar values
 }
 
-void display_question(question *questions, char *category, int value)
+void display_question(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES], char *category, int value)
 {
     // Implement display of a specific question
 }
 
-bool valid_answer(question *questions, char *category, int value, char *answer)
+bool valid_answer(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES], char *category, int value, char *answer)
 {
     // Implement answer validation logic
     return false;
 }
 
-bool already_answered(question *questions, char *category, int value)
+bool already_answered(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES], char *category, int value)
 {
     // Implement check if the question has already been answered
     return false;
