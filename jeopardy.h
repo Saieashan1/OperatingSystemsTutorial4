99bd51291b635668132a19c1d8afd040af9137df
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-20 18:28:38 -0500

    Update jeopardy.h

--- jeopardy.h
+++ jeopardy.h
@@ -6,7 +6,20 @@
 #include "questions.h"
 #include "players.h"
 
-extern void tokenize(char *input, char **tokens);
-extern void show_results(player *players);
+#define NUM_PLAYERS 4
+#define MAX_PLAYER_NAME_LEN 256
+#define MAX_QUESTION_LEN 256
+#define MAX_LEN 256
+
+// Function declarations
+extern void initialize_game(question *questions);
+extern void display_categories(question *questions);
+extern void display_question(question *questions, char *category, int value);
+extern int get_player_index(player *players, int num_players, char *name);
+extern int get_question_index(question *questions, char *category, int value);
+extern int is_question_answered(question *questions, int index);
+extern void mark_question_answered(question *questions, int index);
+extern int is_game_over(question *questions);
+extern void show_results(player *players, int num_players);
 
 #endif /* JEOPARDY_H_ */
