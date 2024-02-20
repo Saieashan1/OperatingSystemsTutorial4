d16da332458a55f7e359a3a7bf78e159a2004475
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-20 18:45:59 -0500

    Create categories.h

---
+++ categories.h
@@ -0,0 +1,11 @@
+/* categories.h */
+
+#ifndef CATEGORIES_H_
+#define CATEGORIES_H_
+
+#define MAX_CATEGORY_LEN 256
+#define NUM_CATEGORIES 5  // Change to your desired value
+
+// Function prototypes (if needed)
+
+#endif /* CATEGORIES_H_ */
