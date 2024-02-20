cbd225cb10b1d430671ff7149c4d96e16d240cf5
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-20 18:30:08 -0500

    Update players.h

--- players.h
+++ players.h
@@ -5,16 +5,16 @@
 
 #include <stdbool.h>
 
-#define MAX_PLAYER_NAME_LEN 30
-#define NUM_PLAYERS 4
+#define MAX_LEN 256
 
+// Player struct for each player
 typedef struct {
-    char name[MAX_PLAYER_NAME_LEN];
+    char name[MAX_LEN];
     int score;
 } player;
 
-extern bool player_exists(player *players, char *name);
-extern void update_score(player *players, char *name, int score);
-extern void initialize_player(player *player, char *name);
+// Function prototypes
+extern bool player_exists(player *players, int num_players, char *name);
+extern void update_score(player *players, int num_players, char *name, int score);
 
 #endif /* PLAYERS_H_ */
