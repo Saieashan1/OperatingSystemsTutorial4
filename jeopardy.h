002ff05611d90046054db9f5a5192fa233c3e4a4
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-20 19:04:00 -0500

    Update jeopardy.h

--- jeopardy.h
+++ jeopardy.h
@@ -1,25 +1,10 @@
-/* jeopardy.h */
-
-#ifndef JEOPARDY_H_
-#define JEOPARDY_H_
+#ifndef JEOPARDY_H
+#define JEOPARDY_H
 
 #include "questions.h"
 #include "players.h"
 
-#define NUM_PLAYERS 4
-#define MAX_PLAYER_NAME_LEN 256
-#define MAX_QUESTION_LEN 256
-#define MAX_LEN 256
+void tokenize(char *input, char **tokens);
+void show_results(Player *players);
 
-// Function declarations
-extern void initialize_game(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES]);
-extern void display_categories(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES]);
-extern void display_question(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES], char *category, int value);
-extern int get_player_index(player *players, int num_players, char *name);
-extern int get_question_index(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES], char *category, int value);
-extern int is_question_answered(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES], int index);
-extern void mark_question_answered(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES], int index);
-extern int is_game_over(question questions[MAX_CATEGORIES][NUM_DOLLAR_VALUES]);
-extern void show_results(player *players, int num_players);
-
-#endif /* JEOPARDY_H_ */
+#endif
