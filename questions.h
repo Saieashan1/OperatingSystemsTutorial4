14adf6718fbce313ba59ec32a6bfb08a92bd3176
Suluseahawks <145396681+Suluseahawks@users.noreply.github.com>
2024-02-20 19:58:52 -0500

    Update questions.h

--- questions.h
+++ questions.h
@@ -1,8 +1,6 @@
 #ifndef QUESTIONS_H
 #define QUESTIONS_H
 
-#include <stdbool.h>
-
 typedef struct {
     char category[50];
     char question[200];
@@ -14,7 +12,9 @@ typedef struct {
 void initialize_game(Question *questions);
 void display_categories(Question *questions);
 void display_question(Question *questions, char *category, int value);
-bool valid_answer(Question *questions, char *category, int value, char *answer);
-bool already_answered(Question *questions, char *category, int value);
+int determine_question_index(Question *questions, char *category, int value);
+int valid_answer(Question *questions, char *category, int value, char *answer);
+int already_answered(Question *questions, char *category, int value);
 
 #endif
+
